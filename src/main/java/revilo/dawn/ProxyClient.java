3d186471ccc5d10951bd9ffbf25a690542d84f0b
Relivof <oli.facey5@gmail.com>
2024-11-28 20:21:43 +1100

    stuff

---
+++ src/main/java/revilo/dawn/ProxyClient.java
@@ -0,0 +1,11 @@
+package revilo.dawn;
+
+import gregapi.api.Abstract_Proxy;
+
+
+/**
+ * @author Revilo
+ */
+public final class ProxyClient extends Abstract_Proxy {
+	// Insert your Clientside-only implementation of Stuff here
+}
---
+++ src/main/java/revilo/dawn/ProxyServer.java
@@ -0,0 +1,11 @@
+package revilo.dawn;
+
+import gregapi.api.Abstract_Proxy;
+
+
+/**
+ * @author Revilo
+ */
+public final class ProxyServer extends Abstract_Proxy {
+	// Insert your Serverside-only implementation of Stuff here
+}
